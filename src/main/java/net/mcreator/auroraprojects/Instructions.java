64cfe18dddc1813c93fc9bb89a3f5d8db244b79c
Volteriun <unconfigured@null.spigotmc.org>
2021-09-28 19:01:01 +0200

    Instructions

---
+++ src/main/java/net/mcreator/auroraprojects/Instructions.java
@@ -0,0 +1,23 @@
+@Volte  :
+-Tu fait les cables et les machines et le systeme electrique...etc
+  (et le teleporteur tier 3)
+-Ensuite après avoir fait sa et seulement avoir fait ça tu fais le driller.
+-Après le driller tu fait Atyotis et tu fait le masque a gaz par la même 
+ occasion pour la planète
+Dès que tu as fini une tache tu nous l'informe et si tu as une idée qui te vien tu nous la dis, on voit pour l'ajouter au projet et tu l'ajoute quand tu as fini toutes tes taches
+
+
+@ContUpsilon ContUpsilon (moi):
+-Je fais les planètes de Tier 1 et 2 (les ecosystemes, mobs, outils...etc)
+-Je m'occupe des teleporteurs t1 et t2
+-Je m'occupe de créé un bestiaire a la fin de toutes mes taches
+-ensuite j'optimise et je perfectionne et règles TOUT les bugs et les fautes.
+
+@?v?? :
+- Après avoir fait le boss golem et le gobelin en plus grand tu peut créé des mobs pour les planètes et leurs modèles 3D en nous expliquant c'est quoi et on les ajoutes dans le jeu
+
+@Gege , @AdessamaJr :
+-Vous pouvez imaginez des creatures passives ou aggresives pour les planètes et nous passer les modèles 3D aussi
+
+@LumineOff , @?? | Idzykato , @YoKo 
+-Si vous participer encore au projet, dîtes moi qu'est ce que vous pourrez faire en mp pour nous aider ducoup et dire si vous êtes specialiser en creation de modèles 3D en Mcreator ou en textures
