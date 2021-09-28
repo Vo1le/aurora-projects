53f4f861dadc65b90fdb7ca225a8b7820bfe4d09
noedartois12 <darto@DESKTOP-A4ESV6K.home>
2021-09-28 21:14:23 +0200

    ajoutMillenus

--- src/main/java/net/mcreator/auroraprojects/block/HerbedeMillenusBlock.java
+++ src/main/java/net/mcreator/auroraprojects/block/HerbedeMillenusBlock.java
@@ -2,17 +2,16 @@
 package net.mcreator.auroraprojects.block;
 
 import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
 
 import net.minecraft.world.IBlockReader;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.Direction;
 import net.minecraft.loot.LootContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.WoodButtonBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
@@ -23,11 +22,11 @@ import java.util.List;
 import java.util.Collections;
 
 @AuroraprojectsModElements.ModElement.Tag
-public class MillenusWoodPackButtonBlock extends AuroraprojectsModElements.ModElement {
-	@ObjectHolder("auroraprojects:millenus_wood_pack_button")
+public class HerbedeMillenusBlock extends AuroraprojectsModElements.ModElement {
+	@ObjectHolder("auroraprojects:herbede_millenus")
 	public static final Block block = null;
-	public MillenusWoodPackButtonBlock(AuroraprojectsModElements instance) {
-		super(instance, 11);
+	public HerbedeMillenusBlock(AuroraprojectsModElements instance) {
+		super(instance, 23);
 	}
 
 	@Override
@@ -36,15 +35,16 @@ public class MillenusWoodPackButtonBlock extends AuroraprojectsModElements.ModEl
 		elements.items
 				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
 	}
-	public static class CustomBlock extends WoodButtonBlock {
+	public static class CustomBlock extends Block {
 		public CustomBlock() {
-			super(Block.Properties.create(Material.WOOD).sound(SoundType.WOOD).hardnessAndResistance(2f, 3f).setLightLevel(s -> 0));
-			setRegistryName("millenus_wood_pack_button");
+			super(Block.Properties.create(Material.ORGANIC).sound(SoundType.GROUND).hardnessAndResistance(1f, 1f).setLightLevel(s -> 0)
+					.harvestLevel(1).harvestTool(ToolType.SHOVEL).setRequiresTool());
+			setRegistryName("herbede_millenus");
 		}
 
 		@Override
-		public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
-			return 5;
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
 		}
 
 		@Override
