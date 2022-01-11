18be9497023206c5dadb51c6a8e909f4bff5ad3d
Volteriun <unconfigured@null.spigotmc.org>
2022-01-11 19:05:33 +0100

    Patch+totem commencement

--- src/main/java/net/mcreator/auroraprojects/procedures/BoomProcedure.java
+++ src/main/java/net/mcreator/auroraprojects/procedures/BoomProcedure.java
@@ -3,11 +3,16 @@ package net.mcreator.auroraprojects.procedures;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
+import net.minecraft.world.GameType;
 import net.minecraft.world.Explosion;
 import net.minecraft.particles.ParticleTypes;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.client.network.play.NetworkPlayerInfo;
+import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
+import net.minecraft.client.Minecraft;
 
 import net.mcreator.auroraprojects.item.BbulletItem;
 import net.mcreator.auroraprojects.AuroraprojectsMod;
@@ -50,32 +55,45 @@ public class BoomProcedure {
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		Entity entity = (Entity) dependencies.get("entity");
 		double localnumber = 0;
-		localnumber = ((new Random()).nextInt((int) 10 + 1));
-		if (localnumber == 3) {
-			if (world instanceof World && !((World) world).isRemote) {
-				((World) world).createExplosion(null, (int) x, (int) y, (int) z, (float) 2, Explosion.Mode.NONE);
-			}
-		}
-		if (localnumber == 5) {
-			if (entity instanceof LivingEntity) {
-				Entity _ent = entity;
-				if (!_ent.world.isRemote()) {
-					BbulletItem.shoot(_ent.world, (LivingEntity) entity, new Random(), (float) 1, (float) 5, (int) 1);
-				}
-			}
-			for (int index0 = 0; index0 < (int) (10); index0++) {
-				if (world instanceof ServerWorld) {
-					((ServerWorld) world).spawnParticle(ParticleTypes.SPIT, x, y, z, (int) 50, 5, 10, 5, 0.01);
-				}
-			}
-		}
-		if (localnumber == 8) {
-			{
-				Entity _ent = entity;
-				_ent.setPositionAndUpdate(x, (y + 10), z);
+		if (new Object() {
+			public boolean checkGamemode(Entity _ent) {
 				if (_ent instanceof ServerPlayerEntity) {
-					((ServerPlayerEntity) _ent).connection.setPlayerLocation(x, (y + 10), z, _ent.rotationYaw, _ent.rotationPitch,
-							Collections.emptySet());
+					return ((ServerPlayerEntity) _ent).interactionManager.getGameType() == GameType.SURVIVAL;
+				} else if (_ent instanceof PlayerEntity && _ent.world.isRemote()) {
+					NetworkPlayerInfo _npi = Minecraft.getInstance().getConnection()
+							.getPlayerInfo(((AbstractClientPlayerEntity) _ent).getGameProfile().getId());
+					return _npi != null && _npi.getGameType() == GameType.SURVIVAL;
+				}
+				return false;
+			}
+		}.checkGamemode(entity)) {
+			localnumber = ((new Random()).nextInt((int) 10 + 1));
+			if (localnumber == 3) {
+				if (world instanceof World && !((World) world).isRemote) {
+					((World) world).createExplosion(null, (int) x, (int) y, (int) z, (float) 2, Explosion.Mode.NONE);
+				}
+			}
+			if (localnumber == 5) {
+				if (entity instanceof LivingEntity) {
+					Entity _ent = entity;
+					if (!_ent.world.isRemote()) {
+						BbulletItem.shoot(_ent.world, (LivingEntity) entity, new Random(), (float) 1, (float) 5, (int) 1);
+					}
+				}
+				for (int index0 = 0; index0 < (int) (10); index0++) {
+					if (world instanceof ServerWorld) {
+						((ServerWorld) world).spawnParticle(ParticleTypes.SPIT, x, y, z, (int) 50, 5, 10, 5, 0.01);
+					}
+				}
+			}
+			if (localnumber == 8) {
+				{
+					Entity _ent = entity;
+					_ent.setPositionAndUpdate(x, (y + 10), z);
+					if (_ent instanceof ServerPlayerEntity) {
+						((ServerPlayerEntity) _ent).connection.setPlayerLocation(x, (y + 10), z, _ent.rotationYaw, _ent.rotationPitch,
+								Collections.emptySet());
+					}
 				}
 			}
 		}
