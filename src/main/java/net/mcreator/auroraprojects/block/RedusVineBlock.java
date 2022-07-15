2d4c51b54ba64efab9674660d36f1a3d792a33f4
vorlox <64790334+Evan0707@users.noreply.github.com>
2022-07-15 11:02:50 +0200

    growthPlante

---
+++ src/main/java/net/mcreator/auroraprojects/block/RedusVineBlock.java
@@ -0,0 +1,103 @@
+
+package net.mcreator.auroraprojects.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.common.PlantType;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SugarCaneBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.auroraprojects.itemgroup.AtyotisTabItemGroup;
+import net.mcreator.auroraprojects.AuroraprojectsModElements;
+
+import java.util.Random;
+import java.util.List;
+import java.util.Collections;
+
+@AuroraprojectsModElements.ModElement.Tag
+public class RedusVineBlock extends AuroraprojectsModElements.ModElement {
+	@ObjectHolder("auroraprojects:redus_vine")
+	public static final Block block = null;
+
+	public RedusVineBlock(AuroraprojectsModElements instance) {
+		super(instance, 105);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new BlockCustomFlower());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(AtyotisTabItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
+	}
+
+	public static class BlockCustomFlower extends SugarCaneBlock {
+		public BlockCustomFlower() {
+			super(Block.Properties.create(Material.PLANTS).tickRandomly().doesNotBlockMovement().sound(SoundType.PLANT).hardnessAndResistance(0f, 0f)
+					.setLightLevel(s -> 0));
+			setRegistryName("redus_vine");
+		}
+
+		@Override
+		public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+			return 100;
+		}
+
+		@Override
+		public int getFireSpreadSpeed(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
+			return 60;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+
+		@Override
+		public PlantType getPlantType(IBlockReader world, BlockPos pos) {
+			return PlantType.PLAINS;
+		}
+
+		@Override
+		public void tick(BlockState blockstate, ServerWorld world, BlockPos pos, Random random) {
+			if (!blockstate.isValidPosition(world, pos)) {
+				world.destroyBlock(pos, true);
+			} else if (world.isAirBlock(pos.up())) {
+				int i = 1;
+				for (; world.getBlockState(pos.down(i)).getBlock() == this; ++i);
+				if (i < 6) {
+					int j = blockstate.get(AGE);
+					if (j == 15) {
+						world.setBlockState(pos.up(), getDefaultState());
+						world.setBlockState(pos, blockstate.with(AGE, 0), 4);
+					} else {
+						world.setBlockState(pos, blockstate.with(AGE, j + 1), 4);
+					}
+				}
+			}
+		}
+	}
+}
