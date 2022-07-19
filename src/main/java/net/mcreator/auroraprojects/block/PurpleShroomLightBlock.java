962e9caf1322ad9cb01c4b5549127b3ac24ee21d
vorlox <64790334+Evan0707@users.noreply.github.com>
2022-07-19 14:22:36 +0200

    add

---
+++ src/main/java/net/mcreator/auroraprojects/block/PurpleShroomLightBlock.java
@@ -0,0 +1,57 @@
+
+package net.mcreator.auroraprojects.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.auroraprojects.itemgroup.AtyotisTabItemGroup;
+import net.mcreator.auroraprojects.AuroraprojectsModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@AuroraprojectsModElements.ModElement.Tag
+public class PurpleShroomLightBlock extends AuroraprojectsModElements.ModElement {
+	@ObjectHolder("auroraprojects:purple_shroom_light")
+	public static final Block block = null;
+
+	public PurpleShroomLightBlock(AuroraprojectsModElements instance) {
+		super(instance, 117);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(AtyotisTabItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.WOOL).sound(SoundType.SHROOMLIGHT).hardnessAndResistance(1f, 10f).setLightLevel(s -> 10));
+			setRegistryName("purple_shroom_light");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
