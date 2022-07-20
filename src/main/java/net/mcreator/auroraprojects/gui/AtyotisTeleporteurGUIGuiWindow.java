fe2cd6255130046b6e31bb908d4109bb282a9d0d
vorlox <64790334+Evan0707@users.noreply.github.com>
2022-07-20 14:39:40 +0200

    Fix teleportation

---
+++ src/main/java/net/mcreator/auroraprojects/gui/AtyotisTeleporteurGUIGuiWindow.java
@@ -0,0 +1,98 @@
+
+package net.mcreator.auroraprojects.gui;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.client.gui.screen.inventory.ContainerScreen;
+import net.minecraft.client.Minecraft;
+
+import net.mcreator.auroraprojects.AuroraprojectsMod;
+
+import java.util.HashMap;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.matrix.MatrixStack;
+
+@OnlyIn(Dist.CLIENT)
+public class AtyotisTeleporteurGUIGuiWindow extends ContainerScreen<AtyotisTeleporteurGUIGui.GuiContainerMod> {
+	private World world;
+	private int x, y, z;
+	private PlayerEntity entity;
+	private final static HashMap guistate = AtyotisTeleporteurGUIGui.guistate;
+
+	public AtyotisTeleporteurGUIGuiWindow(AtyotisTeleporteurGUIGui.GuiContainerMod container, PlayerInventory inventory, ITextComponent text) {
+		super(container, inventory, text);
+		this.world = container.world;
+		this.x = container.x;
+		this.y = container.y;
+		this.z = container.z;
+		this.entity = container.entity;
+		this.xSize = 176;
+		this.ySize = 166;
+	}
+
+	private static final ResourceLocation texture = new ResourceLocation("auroraprojects:textures/atyotis_teleporteur_gui.png");
+
+	@Override
+	public void render(MatrixStack ms, int mouseX, int mouseY, float partialTicks) {
+		this.renderBackground(ms);
+		super.render(ms, mouseX, mouseY, partialTicks);
+		this.renderHoveredTooltip(ms, mouseX, mouseY);
+	}
+
+	@Override
+	protected void drawGuiContainerBackgroundLayer(MatrixStack ms, float partialTicks, int gx, int gy) {
+		RenderSystem.color4f(1, 1, 1, 1);
+		RenderSystem.enableBlend();
+		RenderSystem.defaultBlendFunc();
+		Minecraft.getInstance().getTextureManager().bindTexture(texture);
+		int k = (this.width - this.xSize) / 2;
+		int l = (this.height - this.ySize) / 2;
+		this.blit(ms, k, l, 0, 0, this.xSize, this.ySize, this.xSize, this.ySize);
+		RenderSystem.disableBlend();
+	}
+
+	@Override
+	public boolean keyPressed(int key, int b, int c) {
+		if (key == 256) {
+			this.minecraft.player.closeScreen();
+			return true;
+		}
+		return super.keyPressed(key, b, c);
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+	}
+
+	@Override
+	protected void drawGuiContainerForegroundLayer(MatrixStack ms, int mouseX, int mouseY) {
+	}
+
+	@Override
+	public void onClose() {
+		super.onClose();
+		Minecraft.getInstance().keyboardListener.enableRepeatEvents(false);
+	}
+
+	@Override
+	public void init(Minecraft minecraft, int width, int height) {
+		super.init(minecraft, width, height);
+		minecraft.keyboardListener.enableRepeatEvents(true);
+		this.addButton(new Button(this.guiLeft + 59, this.guiTop + 88, 61, 20, new StringTextComponent("ATYOTIS"), e -> {
+			if (true) {
+				AuroraprojectsMod.PACKET_HANDLER.sendToServer(new AtyotisTeleporteurGUIGui.ButtonPressedMessage(0, x, y, z));
+				AtyotisTeleporteurGUIGui.handleButtonAction(entity, 0, x, y, z);
+			}
+		}));
+	}
+}
---
+++ src/main/java/net/mcreator/auroraprojects/procedures/TeleportationAtyotisProcedure.java
@@ -0,0 +1,75 @@
+package net.mcreator.auroraprojects.procedures;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.network.play.server.SPlayerAbilitiesPacket;
+import net.minecraft.network.play.server.SPlaySoundEventPacket;
+import net.minecraft.network.play.server.SPlayEntityEffectPacket;
+import net.minecraft.network.play.server.SChangeGameStatePacket;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.auroraprojects.AuroraprojectsMod;
+
+import java.util.Map;
+
+public class TeleportationAtyotisProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				AuroraprojectsMod.LOGGER.warn("Failed to load dependency world for procedure TeleportationAtyotis!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				AuroraprojectsMod.LOGGER.warn("Failed to load dependency x for procedure TeleportationAtyotis!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				AuroraprojectsMod.LOGGER.warn("Failed to load dependency y for procedure TeleportationAtyotis!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				AuroraprojectsMod.LOGGER.warn("Failed to load dependency z for procedure TeleportationAtyotis!");
+			return;
+		}
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				AuroraprojectsMod.LOGGER.warn("Failed to load dependency entity for procedure TeleportationAtyotis!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		Entity entity = (Entity) dependencies.get("entity");
+		{
+			Entity _ent = entity;
+			if (!_ent.world.isRemote && _ent instanceof ServerPlayerEntity) {
+				RegistryKey<World> destinationType = RegistryKey.getOrCreateKey(Registry.WORLD_KEY, new ResourceLocation("auroraprojects:atyotis"));
+				ServerWorld nextWorld = _ent.getServer().getWorld(destinationType);
+				if (nextWorld != null) {
+					((ServerPlayerEntity) _ent).connection.sendPacket(new SChangeGameStatePacket(SChangeGameStatePacket.field_241768_e_, 0));
+					((ServerPlayerEntity) _ent).teleport(nextWorld, nextWorld.getSpawnPoint().getX(), nextWorld.getSpawnPoint().getY() + 1,
+							nextWorld.getSpawnPoint().getZ(), _ent.rotationYaw, _ent.rotationPitch);
+					((ServerPlayerEntity) _ent).connection.sendPacket(new SPlayerAbilitiesPacket(((ServerPlayerEntity) _ent).abilities));
+					for (EffectInstance effectinstance : ((ServerPlayerEntity) _ent).getActivePotionEffects()) {
+						((ServerPlayerEntity) _ent).connection.sendPacket(new SPlayEntityEffectPacket(_ent.getEntityId(), effectinstance));
+					}
+					((ServerPlayerEntity) _ent).connection.sendPacket(new SPlaySoundEventPacket(1032, BlockPos.ZERO, 0, false));
+				}
+			}
+		}
+		world.destroyBlock(new BlockPos((int) x, (int) y, (int) z), false);
+		world.destroyBlock(new BlockPos((int) x, (int) (y - 1), (int) z), false);
+	}
+}
