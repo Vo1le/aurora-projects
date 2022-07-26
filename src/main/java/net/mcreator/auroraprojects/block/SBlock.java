a07ed21525916cba9e551b7a2430a1ddc8e7f95c
vorlox <64790334+Evan0707@users.noreply.github.com>
2022-07-26 15:58:05 +0200

    plank

---
+++ src/main/java/net/mcreator/auroraprojects/block/SBlock.java
@@ -0,0 +1,63 @@
+
+package net.mcreator.auroraprojects.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.PaneBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.auroraprojects.itemgroup.AuroraTeleporteursItemGroup;
+import net.mcreator.auroraprojects.AuroraprojectsModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@AuroraprojectsModElements.ModElement.Tag
+public class SBlock extends AuroraprojectsModElements.ModElement {
+	@ObjectHolder("auroraprojects:s")
+	public static final Block block = null;
+
+	public SBlock(AuroraprojectsModElements instance) {
+		super(instance, 154);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(
+				() -> new BlockItem(block, new Item.Properties().group(AuroraTeleporteursItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutoutMipped());
+	}
+
+	public static class CustomBlock extends PaneBlock {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0));
+			setRegistryName("s");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
