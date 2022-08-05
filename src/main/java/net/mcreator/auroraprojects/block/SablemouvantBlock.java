c37054af6bc5b90611096fb046a9a890d9bf1d99
ContUpsilon <83365546+ContUpsilon@users.noreply.github.com>
2022-08-05 15:54:36 +0200

    ajout sable mouvant
    t

---
+++ src/main/java/net/mcreator/auroraprojects/block/SablemouvantBlock.java
@@ -0,0 +1,183 @@
+
+package net.mcreator.auroraprojects.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fluids.ForgeFlowingFluid;
+import net.minecraftforge.fluids.FluidAttributes;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.common.property.Properties;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.gen.placement.Placement;
+import net.minecraft.world.gen.placement.ChanceConfig;
+import net.minecraft.world.gen.feature.LakesFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.BlockStateFeatureConfig;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.World;
+import net.minecraft.world.ISeedReader;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.registry.WorldGenRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.state.StateContainer;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BucketItem;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.FlowingFluid;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.FlowingFluidBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.auroraprojects.AuroraprojectsModElements;
+
+import java.util.Random;
+
+@AuroraprojectsModElements.ModElement.Tag
+public class SablemouvantBlock extends AuroraprojectsModElements.ModElement {
+	@ObjectHolder("auroraprojects:sablemouvant")
+	public static final FlowingFluidBlock block = null;
+	@ObjectHolder("auroraprojects:sablemouvant_bucket")
+	public static final Item bucket = null;
+	public static FlowingFluid flowing = null;
+	public static FlowingFluid still = null;
+	private ForgeFlowingFluid.Properties fluidproperties = null;
+
+	public SablemouvantBlock(AuroraprojectsModElements instance) {
+		super(instance, 178);
+		FMLJavaModLoadingContext.get().getModEventBus().register(new FluidRegisterHandler());
+		MinecraftForge.EVENT_BUS.register(this);
+		FMLJavaModLoadingContext.get().getModEventBus().register(new FeatureRegisterHandler());
+	}
+
+	private static class FluidRegisterHandler {
+		@SubscribeEvent
+		public void registerFluids(RegistryEvent.Register<Fluid> event) {
+			event.getRegistry().register(still);
+			event.getRegistry().register(flowing);
+		}
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(still, RenderType.getTranslucent());
+		RenderTypeLookup.setRenderLayer(flowing, RenderType.getTranslucent());
+	}
+
+	@Override
+	public void initElements() {
+		fluidproperties = new ForgeFlowingFluid.Properties(() -> still, () -> flowing,
+				FluidAttributes
+						.builder(new ResourceLocation("auroraprojects:blocks/red_sand"), new ResourceLocation("auroraprojects:blocks/red_sand"))
+						.luminosity(0).density(1000).viscosity(1000).temperature(300)
+
+						.rarity(Rarity.UNCOMMON)).explosionResistance(100f)
+
+								.tickRate(35).levelDecreasePerBlock(1).slopeFindDistance(4).bucket(() -> bucket).block(() -> block);
+		still = (FlowingFluid) new CustomFlowingFluid.Source(fluidproperties).setRegistryName("sablemouvant");
+		flowing = (FlowingFluid) new CustomFlowingFluid.Flowing(fluidproperties).setRegistryName("sablemouvant_flowing");
+		elements.blocks
+				.add(() -> new FlowingFluidBlock(still, Block.Properties.create(Material.WATER).hardnessAndResistance(100f).setLightLevel(s -> 0)) {
+					@Override
+					public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+						return true;
+					}
+				}.setRegistryName("sablemouvant"));
+		elements.items.add(() -> new BucketItem(still,
+				new Item.Properties().containerItem(Items.BUCKET).maxStackSize(1).group(ItemGroup.MISC).rarity(Rarity.UNCOMMON))
+						.setRegistryName("sablemouvant_bucket"));
+	}
+
+	public static abstract class CustomFlowingFluid extends ForgeFlowingFluid {
+		public CustomFlowingFluid(Properties properties) {
+			super(properties);
+		}
+
+		@Override
+		public Vector3d getFlow(IBlockReader world, BlockPos pos, FluidState fluidstate) {
+			return super.getFlow(world, pos, fluidstate).scale(0.05);
+		}
+
+		public static class Source extends CustomFlowingFluid {
+			public Source(Properties properties) {
+				super(properties);
+			}
+
+			public int getLevel(FluidState state) {
+				return 8;
+			}
+
+			public boolean isSource(FluidState state) {
+				return true;
+			}
+		}
+
+		public static class Flowing extends CustomFlowingFluid {
+			public Flowing(Properties properties) {
+				super(properties);
+			}
+
+			protected void fillStateContainer(StateContainer.Builder<Fluid, FluidState> builder) {
+				super.fillStateContainer(builder);
+				builder.add(LEVEL_1_8);
+			}
+
+			public int getLevel(FluidState state) {
+				return state.get(LEVEL_1_8);
+			}
+
+			public boolean isSource(FluidState state) {
+				return false;
+			}
+		}
+	}
+
+	private static Feature<BlockStateFeatureConfig> feature = null;
+	private static ConfiguredFeature<?, ?> configuredFeature = null;
+
+	private static class FeatureRegisterHandler {
+		@SubscribeEvent
+		public void registerFeature(RegistryEvent.Register<Feature<?>> event) {
+			feature = new LakesFeature(BlockStateFeatureConfig.field_236455_a_) {
+				@Override
+				public boolean generate(ISeedReader world, ChunkGenerator generator, Random rand, BlockPos pos, BlockStateFeatureConfig config) {
+					RegistryKey<World> dimensionType = world.getWorld().getDimensionKey();
+					boolean dimensionCriteria = false;
+					if (dimensionType == RegistryKey.getOrCreateKey(Registry.WORLD_KEY, new ResourceLocation("auroraprojects:amires")))
+						dimensionCriteria = true;
+					if (!dimensionCriteria)
+						return false;
+					return super.generate(world, generator, rand, pos, config);
+				}
+			};
+			configuredFeature = feature.withConfiguration(new BlockStateFeatureConfig(block.getDefaultState()))
+					.withPlacement(Placement.WATER_LAKE.configure(new ChanceConfig(5)));
+			event.getRegistry().register(feature.setRegistryName("sablemouvant_lakes"));
+			Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation("auroraprojects:sablemouvant_lakes"), configuredFeature);
+		}
+	}
+
+	@SubscribeEvent
+	public void addFeatureToBiomes(BiomeLoadingEvent event) {
+		event.getGeneration().getFeatures(GenerationStage.Decoration.LOCAL_MODIFICATIONS).add(() -> configuredFeature);
+	}
+}
